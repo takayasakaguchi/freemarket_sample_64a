14254fba868f2e4ac32eedc1752281c4805727ad
kosuke1102 <58392452+kosuke1102@users.noreply.github.com>
2020-03-07 12:48:41 +0900

    メールアドレス

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  # devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_up", :to => "users/registrations#new"
+    # get "sign_out", :to => "users/sessions#destroy" 
+  end
 end
+
+# devise_for :users, :controllers => {
+#   :registrations => 'users/registrations'
+#  }
+ 
+#  devise_scope :user do 
+#   post 'users/sign_up/confirm' => 'users/registrations#confirm'
+#   post 'users/sign_up/complete' => 'users/registrations#complete' 
+#  end
