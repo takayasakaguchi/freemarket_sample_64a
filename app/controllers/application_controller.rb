ebd85015055136aa2f19af45712de2d6841cbbf8
kosuke1102 <58392452+kosuke1102@users.noreply.github.com>
2020-03-14 19:30:45 +0900

    メールアドレス登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :nickname, :password, :password_confirmation, :first_name, :last_name, :first_name_reading, :last_name_reading]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
+
+ 
 end
