50c8e75badb3058db898c17989ed7f1274910220
kosuke1102 <58392452+kosuke1102@users.noreply.github.com>
2020-03-18 22:53:11 +0900

    マークアップ３
    ハッシュロケット修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
   } 
 
   devise_scope :user do
-    get "sign_up", :to => "users/registrations#new"
+    get "sign_up", to: "users/registrations#new"
     # get "sign_out", :to => "users/sessions#destroy" 
   end
 end
