300671afa074cacc330bf402476efd5e2cfd300a
toshi85 <tabibito1207@gmail.com>
2020-03-22 15:21:20 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root to: 'posts#toppage'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
+  } 
+
+  root "purchases#index"
+  resources :signup, only: [:index]
+  resources :credit_card, only: [:index] do
+  end
+  resources :address, only: [:index, :edit] do
+  end
 end
