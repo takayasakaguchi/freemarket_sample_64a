ccf3d6ed515b300ba11689e566736371b20468e7
toshi85 <tabibito1207@gmail.com>
2020-03-22 15:31:45 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-
 ActiveRecord::Schema.define(version: 2020_03_17_124050) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -19,8 +17,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_124050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
+  
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,6 +34,5 @@ ActiveRecord::Schema.define(version: 2020_03_17_124050) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-
+  
 end
