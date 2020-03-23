a7a18e331e6416685302a97aa63ace0fa558c3a1
takayasakaguchi <simple.pips@icloud.com>
2020-03-24 06:40:57 +0900

    自動デプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '18.176.124.191', user: 'ec2-user', roles: %w{app db web}
+server '3.114.180.179', user: 'ec2-user', roles: %w{app db web}
 
 
 
