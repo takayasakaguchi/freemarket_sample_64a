eda5d637ffe79008c5c2da9eed33baa8936b5231
takayasakaguchi <simple.pips@icloud.com>
2020-03-25 00:31:11 +0900

    自動デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth, if: :production?
+ 
   
   protected
 
@@ -12,13 +12,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def production?
-    Rails.env.production?
-  end
+  
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
 end
