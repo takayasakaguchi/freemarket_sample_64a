228d61d1a716723dfe2ddc15f809b3f58c4673e1
kosuke1102 <58392452+kosuke1102@users.noreply.github.com>
2020-04-07 22:54:22 +0900

    パスワード確認用
    パスワード確認用フィールド
    パスワード確認用カラム
    作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_123734) do
+ActiveRecord::Schema.define(version: 2020_04_07_134606) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_123734) do
     t.integer "year"
     t.integer "month"
     t.integer "day"
+    t.string "encrypted_repassword" default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
