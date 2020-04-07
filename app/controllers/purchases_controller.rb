0d83576caf68a3a47006455bbd86edcbc8c2628f
hata-tomoyuki <h.tomo0602@gmail.com>
2020-04-07 19:46:14 +0900

    [住所登録機能]登録情報表示
    purchases/index.html.hamlで住所の情報が確認できるように実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,13 @@
 class PurchasesController < ApplicationController
 
   def index
-
+    user = User.find(1) 
+      #ログイン/ログアウトできないのでダミーデータを作ります
+      
+      
+    @address = user.address
+    @user = User.find(1) 
+    
   end
   
 end
