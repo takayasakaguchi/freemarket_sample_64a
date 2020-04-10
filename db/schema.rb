f29af1eae5bc515c7082bdae388ce37656520bcc
kosuke1102 <58392452+kosuke1102@users.noreply.github.com>
2020-04-10 10:38:49 +0900

    新規登録validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_134606) do
+ActiveRecord::Schema.define(version: 2020_04_09_042346) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_04_07_134606) do
     t.integer "year"
     t.integer "month"
     t.integer "day"
-    t.string "encrypted_repassword" default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
