2a796bc37d56ab21f698c6e5a6af0a1b6e9ae360
hata-tomoyuki <h.tomo0602@gmail.com>
2020-04-19 21:41:42 +0900

    カテゴリー別商品一覧表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,11 @@
 class CategoriesController < ApplicationController
   def show
     @parents = Category.roots.all
-    @parent = Category.roots.all
     @category = Category.find(params[:id])
-    @products = @category.products
+
+    array = @category.descendant_ids
+    array.push(@category.id)
+    
+    @products = Product.where(category_id: array)
   end
 end
