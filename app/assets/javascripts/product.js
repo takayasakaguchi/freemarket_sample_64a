7c1bde6018186246b25d875df8d5c36981f83236
hata-tomoyuki <h.tomo0602@gmail.com>
2020-04-25 21:06:34 +0900

    jsファイルインデント修正

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -47,34 +47,34 @@ $(function(){
     });
   });
 
-  //削除ボタンをクリックすると発火するイベント
-$(document).on("click", '.item-image__operetion--delete', function(){
-  //削除を押されたプレビュー要素を取得
-  var target_image = $(this).parent().parent()
-  //削除を押されたプレビューimageのfile名を取得
-  var target_name = $(target_image).data('image')
-  //プレビューがひとつだけの場合、file_fieldをクリア
-  if(file_field.files.length==1){
-    //inputタグに入ったファイルを削除
-    $('input[type=file]').val(null)
-    dataBox.clearData();
-    console.log(dataBox)
-  }else{
-    //プレビューが複数の場合
-    $.each(file_field.files, function(i,input){
-      //削除を押された要素と一致した時、index番号に基づいてdataBoxに格納された要素を削除する
-      if(input.name==target_name){
-        dataBox.items.remove(i) 
-      }
-    })
-    //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に再度代入
-    file_field.files = dataBox.files
-  }
-  //プレビューを削除
-  target_image.remove()
-  //image-box__containerクラスをもつdivタグのクラスを削除のたびに変更
-  var num = $('.item-image').length
-  $('#image-box__container').show()
-  $('#image-box__container').attr('class', `item-num-${num}`)
-})
+    //削除ボタンをクリックすると発火するイベント
+  $(document).on("click", '.item-image__operetion--delete', function(){
+    //削除を押されたプレビュー要素を取得
+    var target_image = $(this).parent().parent()
+    //削除を押されたプレビューimageのfile名を取得
+    var target_name = $(target_image).data('image')
+    //プレビューがひとつだけの場合、file_fieldをクリア
+    if(file_field.files.length==1){
+      //inputタグに入ったファイルを削除
+      $('input[type=file]').val(null)
+      dataBox.clearData();
+      console.log(dataBox)
+    }else{
+      //プレビューが複数の場合
+      $.each(file_field.files, function(i,input){
+        //削除を押された要素と一致した時、index番号に基づいてdataBoxに格納された要素を削除する
+        if(input.name==target_name){
+          dataBox.items.remove(i) 
+        }
+      })
+      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に再度代入
+      file_field.files = dataBox.files
+    }
+    //プレビューを削除
+    target_image.remove()
+    //image-box__containerクラスをもつdivタグのクラスを削除のたびに変更
+    var num = $('.item-image').length
+    $('#image-box__container').show()
+    $('#image-box__container').attr('class', `item-num-${num}`)
+  })
 });
