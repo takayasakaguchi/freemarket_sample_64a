9814dbe93562a80b2236dad716bc22482c55cad3
hata-tomoyuki <h.tomo0602@gmail.com>
2020-04-26 17:08:22 +0900

    カテゴリー選択機能の不要コード削除
    使わないコードを削除
    jsファイルとエラーハンドリング修正

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -36,39 +36,33 @@ $(function(){
   $('#parent_category').on('change', function(){
     // console.log("r")
     var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
-    if (parentCategory != "---"){ //親カテゴリーが初期値でないことを確認
-      $.ajax({
-        url: 'get_category_children',
-        type: 'GET',
-        data: { parent_name: parentCategory },
-        dataType: 'json'
-      })
-      .done(function(children){
-        // console.log(children)
-        $('#children_wrapper').remove(); //親が変更された時、子以下を削除するする
-        $('#grandchildren_wrapper').remove();
-        $('#size_wrapper').remove();
-        $('#brand_wrapper').remove();
-        var insertHTML = '';
-        children.forEach(function(child){
-          insertHTML += appendOption(child);
-        });
-        appendChidrenBox(insertHTML);
-      })
-      .fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      })
-    }else{
-      $('#children_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
+    $.ajax({
+      url: 'get_category_children',
+      type: 'GET',
+      data: { parent_name: parentCategory },
+      dataType: 'json'
+    })
+    .done(function(children){
+      // console.log(children)
+      $('#children_wrapper').remove(); //親が変更された時、子以下を削除するする
       $('#grandchildren_wrapper').remove();
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
-    }
+      // $('#size_wrapper').remove();
+      // $('#brand_wrapper').remove();
+      var insertHTML = '';
+      children.forEach(function(child){
+        insertHTML += appendOption(child);
+      });
+      appendChidrenBox(insertHTML);
+    })
+    .fail(function(){
+      alert('親カテゴリーを選択してください。');
+      $('#children_wrapper').remove();
+      $('#grandchildren_wrapper').remove();
+    })
   });
   // 子カテゴリー選択後のイベント
   $('.listing-product-detail__category').on('change', '#child_category', function(){
     var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
-    console.log(childId)
     if (childId != "---"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
         url: 'get_category_grandchildren',
@@ -80,8 +74,6 @@ $(function(){
         // console.log(childId)
         if (grandchildren.length != 0) {
           $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除するする
-          $('#size_wrapper').remove();
-          $('#brand_wrapper').remove();
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
@@ -90,12 +82,12 @@ $(function(){
         }
       })
       .fail(function(){
-        alert('カテゴリー取得に失敗しました');
+        alert('親カテゴリーの選択からやり直してください');
+        $('#children_wrapper').remove();
+        $('#grandchildren_wrapper').remove();
       })
     }else{
       $('#grandchildren_wrapper').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
     }
   });
 })
