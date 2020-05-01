e4d4b54349ca425de45300c905803444ab5b69a9
takayasakaguchi <simple.pips@icloud.com>
2020-05-01 20:11:08 +0900

    clumm

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_04_15_125405) do
+ActiveRecord::Schema.define(version: 2020_04_19_115759) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -47,21 +44,32 @@ ActiveRecord::Schema.define(version: 2020_04_15_125405) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description", null: false
+    t.string "brand"
+    t.string "status", null: false
+    t.string "postage", null: false
+    t.string "destination", null: false
+    t.integer "lead_time", null: false
+    t.string "size"
+    t.integer "purchase"
+    t.integer "buyer_id"
+    t.integer "user_id", null: false
+    t.integer "category_id", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_reading", null: false
+    t.string "last_name_reading", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "first_name_reading"
-    t.string "last_name_reading"
     t.integer "year"
     t.integer "month"
     t.integer "day"
