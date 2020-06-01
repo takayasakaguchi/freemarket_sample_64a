05d1ff074d786bf9a2ed33784743fa4246aa3f65
hata-tomoyuki <h.tomo0602@gmail.com>
2020-06-01 20:04:14 +0900

    新規登録フォーム修正
    例）メルカリ太郎　から
    例）フリマ太郎　に変更

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -2,7 +2,7 @@ $(function() {
 
   $("#product_price").on("keyup", function() {
     var priceBeforeTax = $("#product_price").val();
-    var input = priceBeforeTax * 0.1;
+    var input = Math.floor(priceBeforeTax * 0.1);
     if (priceBeforeTax>=300&&priceBeforeTax<=999999999){
       $('.single-main__form1__text4__commission__from__boder').html('¥' + input);
     }else{
@@ -16,7 +16,7 @@ $(function() {
 $("#product_price").on("keyup", function() {
     var priceBeforeTax = $("#product_price").val();
     var input = priceBeforeTax * 0.1;
-    var Profit = priceBeforeTax - input;
+    var Profit = Math.floor(priceBeforeTax - input);
     if (priceBeforeTax>=300&&priceBeforeTax<=999999999){
     $('.single-main__form1__text4__commission__from2__benefit__boder').html('¥' + Profit);
     }else{
